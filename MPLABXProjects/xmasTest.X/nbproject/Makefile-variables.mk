12ed6eedce32cffe42fd84fee797df246af1b6ba
cedd82 <cwarnant@gmail.com>
2023-01-29 14:27:56 +1100

    '123'

---
+++ MPLABXProjects/xmasTest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=xmasTest.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/xmasTest.X.production.hex
